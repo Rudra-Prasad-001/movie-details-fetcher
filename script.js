fcecd4429f0468e7013209754df411e969bc3a97
Rudra Prasad Pati <148573027+Rudra-Prasad-001@users.noreply.github.com>
2024-12-31 11:47:48 +0000

    Added animation for better user experience

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ submit.addEventListener('click', getMovieDetails);
 function getMovieDetails() {
     const movieTitle = String(document.querySelector('.movie-title').value).trim();
     const dialog = document.querySelector('.dialog');
+    const loading = document.querySelector('.loading');
     const showDetails = document.querySelector('.show-details');
     const showTitle = document.querySelector('.show-title');
     const showCrew = document.querySelector('.show-crew');
@@ -20,11 +21,17 @@ function getMovieDetails() {
     if (movieTitle === '') {
         dialog.innerHTML = `Please enter a valid movie name!`;
         showDetails.classList.add('hidden'); // Hide details if input is invalid
+        loading.classList.add('hidden'); // Hide loading spinner
         return;
     }
 
     const movieName = movieTitle.replaceAll(' ', '+');
-    const url = `https://www.omdbapi.com/?t=${movieName}&apikey=fc5e9a81`; //  My API key
+    const url = `https://www.omdbapi.com/?t=${movieName}&apikey=fc5e9a81`; // my API key
+
+    // Show loading spinner
+    loading.classList.remove('hidden');
+    dialog.innerHTML = ''; // Clear any previous error messages
+    showDetails.classList.add('hidden'); // Hide details while fetching data
 
     fetch(url)
         .then((response) => {
@@ -34,11 +41,11 @@ function getMovieDetails() {
             return response.json();
         })
         .then((data) => {
+            loading.classList.add('hidden'); // Hide loading spinner once data is fetched
             if (data.Response === 'False') {
                 throw new Error(data.Error);
             }
 
-            dialog.innerHTML = ''; // Clear any previous error messages
             showDetails.classList.remove('hidden'); // Show details if valid movie found
             showTitle.textContent = `Title: ${data.Title}`;
             showCrew.textContent = `Writers: ${data.Writer} | Actors: ${data.Actors}`;
@@ -46,6 +53,7 @@ function getMovieDetails() {
             showRating.textContent = `IMDb Rating: ${data.imdbRating}`;
         })
         .catch((err) => {
+            loading.classList.add('hidden'); // Hide loading spinner in case of error
             dialog.innerHTML = `Oops! Something went wrong. ${err.message}`;
             showDetails.classList.add('hidden'); // Hide details if there's an error
         });
